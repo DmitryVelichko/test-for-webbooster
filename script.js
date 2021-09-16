985303ac61048948a9d6d62e92d4f9b2d2c9b570
Dmitry Velichko <soleil-2005@mail.ru>
2021-09-16 23:14:03 +0300

    New commit

---
+++ script.js
@@ -0,0 +1,42 @@
+const button = document.querySelector(".submit-btn");
+
+const button2 = document.querySelectorAll(".buy-btn");
+
+const wrapper = document.querySelector(".wrapper");
+
+const container = document.querySelector(".container");
+
+button.addEventListener("click", () => {
+  event.preventDefault();
+
+  wrapper.classList.toggle("hide");
+  container.classList.toggle("hide");
+});
+
+button2[0].addEventListener("click", () => {
+  event.preventDefault();
+
+  container.classList.toggle("hide");
+  wrapper.classList.toggle("hide");
+});
+
+button2[1].addEventListener("click", () => {
+  event.preventDefault();
+
+  container.classList.toggle("hide");
+  wrapper.classList.toggle("hide");
+});
+
+button2[2].addEventListener("click", () => {
+  event.preventDefault();
+
+  container.classList.toggle("hide");
+  wrapper.classList.toggle("hide");
+});
+
+button2[3].addEventListener("click", () => {
+  event.preventDefault();
+
+  container.classList.toggle("hide");
+  wrapper.classList.toggle("hide");
+});
